977ac28f5515d4587a8a26823e7bc8ccd52bc6b9
Murodil <murodil@cybertekschool.com>
2022-08-28 15:47:27 -0400

    hello world cucumber test

--- src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
@@ -15,5 +15,19 @@ import static org.junit.Assert.*;
 
 public class HelloWorldApiStepDefs {
 
+    @Given("User sends get request to hello world api")
+    public void user_sends_get_request_to_hello_world_api() {
+
+    }
+
+    @Then("hello world api status code is {int}")
+    public void hello_world_api_status_code_is(Integer int1) {
+
+    }
+
+    @Then("hello world api response body contains {string}")
+    public void hello_world_api_response_body_contains(String string) {
+
+    }
 
 }
