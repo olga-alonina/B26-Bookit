42ac144e21d0db0e5894f30fb26b467c0eebc90c
Murodil <murodil@cybertekschool.com>
2022-08-28 16:53:37 -0400

    hello world cucumber test

--- src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/HelloWorldApiStepDefs.java
@@ -15,20 +15,25 @@ import static org.junit.Assert.*;
 
 public class HelloWorldApiStepDefs {
 
+    public static final Logger LOG = LogManager.getLogger();
+
     String url = ConfigurationReader.getProperty("hello.world.api");
     Response response;
 
     @Given("User sends get request to hello world api")
     public void user_sends_get_request_to_hello_world_api() {
+       LOG.info("Sending GET request to Hello world API = " + url);
+
        response = given().accept(ContentType.JSON)
                 .when().get(url);
 
+       LOG.info("GET request completed with response = " + response.asString());
     }
 
     @Then("hello world api status code is {int}")
     public void hello_world_api_status_code_is(int expStatusCode) {
-        System.out.println("actual status code = " + response.statusCode());
-        System.out.println("expStatusCode = " + expStatusCode);
+        LOG.info("actual status code = " + response.statusCode());
+        LOG.info("expStatusCode = " + expStatusCode);
         assertEquals(expStatusCode , response.statusCode());
     }
 
