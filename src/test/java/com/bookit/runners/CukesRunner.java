c10d494a4776682dfd705be8702ab6589c6b8316
Murodil <murodil@cybertekschool.com>
2022-08-30 21:52:18 -0400

    bookit login feature

--- src/test/java/com/bookit/runners/CukesRunner.java
+++ src/test/java/com/bookit/runners/CukesRunner.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
                   "html:target/default-html-reports"},
         features = "src/test/resources/features/",
         glue = "com/bookit/step_definitions/",
+        tags = "@bookItApi",
         dryRun = false
 )
 public class CukesRunner {
