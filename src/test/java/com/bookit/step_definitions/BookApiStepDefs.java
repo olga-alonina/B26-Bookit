4fc2fcd14a0e480aa82678d33b059aa622071044
Murodil <murodil@cybertekschool.com>
2022-08-30 20:56:14 -0400

    bookit login feature

--- src/test/java/com/bookit/step_definitions/BookApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/BookApiStepDefs.java
@@ -1,28 +1,45 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.BookItApiUtil;
 import com.bookit.utilities.ConfigurationReader;
 import com.bookit.utilities.Environment;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import static org.junit.Assert.*;
+import static io.restassured.RestAssured.*;
 
 public class BookApiStepDefs {
 
     public static final Logger LOG = LogManager.getLogger();
-    String url = Environment.BASE_URL;
+    String baseUrl = Environment.BASE_URL;
+    String accessToken;
+    Response response;
 
     @Given("User logged in to Bookit api as teacher role")
     public void user_logged_in_to_Bookit_api_as_teacher_role() {
         String email = Environment.TEACHER_EMAIL;
         String password = Environment.TEACHER_PASSWORD;
         LOG.info("Authorizing teacher user : email = " + email + ", password = " + password);
+        LOG.info("Environment base url = " + baseUrl);
+
+        accessToken = BookItApiUtil.getAccessToken(email, password);
+
+        if (accessToken == null || accessToken.isEmpty()) {
+            LOG.error("Could not authorize user in authorization server");
+            fail("Could not authorize user in authorization server");
+        }
 
     }
 
     @Given("User sends GET request to {string}")
-    public void user_sends_GET_request_to(String string) {
-
+    public void user_sends_GET_request_to(String endpoint) { ///api/users/me
+        response = given().accept(ContentType.JSON)
+                .and().header("Authorization",  accessToken)
+                .when().get(baseUrl + endpoint);
     }
 
     @Then("status code should be {int}")
